06244c2b363f243f66b58a2d1f1d1cbdbe18b6e9
LoniasGR <avdelasleonidas@gmail.com>
2017-01-18 18:24:47 +0200

    Scheduler works! Only thing left is the handling the input from shell.

--- Operating Systems/Assignment 4/scheduler-shell.c
+++ Operating Systems/Assignment 4/scheduler-shell.c
@@ -28,11 +28,110 @@ struct process;
 
 /************* GLOBAL VARIABLES ******************************/
 
-struct processList* procList;
-struct process* current;
+static struct processList* procList;
+static struct process* current;
 
 /************************************************************/
 
+/* a process has a pNumber given by us, a PID given by the kernel and
+ * the name of the executable that is running.*/
+struct process{
+		int pNumber;
+		pid_t pPid;
+		char* execName;
+		struct process* next;
+};
+
+/* allocates space in the heap for a process struct */
+static struct process*
+create_process(void)
+{
+	struct process* proc;
+	proc = (struct process*) malloc(sizeof(struct process));
+	if (proc == NULL) {
+		perror("create_process");
+		exit(EXIT_FAILURE);
+	}
+	return proc;
+}
+
+/* initializes a process struct to the given id and executable name*/
+static struct process*
+init_process(int number, char* execName)
+{
+		struct process* proc = create_process();
+		proc->pNumber = number;
+		proc->pPid = -1;
+		proc->execName = execName;
+		proc->next = NULL;
+
+		return proc;
+}
+
+/* a struct holding the number of processes and the head of a
+ * process list */
+struct processList {
+		int count;
+		struct process* head;
+};
+
+/* grabs memory from the stack for a processList*/
+static struct processList*
+create_list (void)
+{
+	struct processList* list;
+	list = (struct processList*) malloc(sizeof(struct processList));
+	if (list == NULL) {
+		perror("processList malloc");
+		exit(EXIT_FAILURE);
+	}
+	return list;
+}
+/*initializes an empty process list*/
+static struct processList*
+init_list (void)
+{
+	struct processList* list = create_list();
+	list->count = 0;
+	list->head = NULL;
+	return list;
+}
+
+/* adds a process to the process list */
+static void
+add_proc_to_list (struct processList* list, struct process* proc)
+{
+	list->count +=1;
+	if(list->head == NULL)
+		list->head = proc;
+	else {
+		struct process* temp = list->head;
+		while(temp->next != NULL)
+			temp = temp->next;
+		temp->next = proc;
+	}
+}
+
+/* remove a process from the process list */
+static void
+remove_proc_from_list (struct processList* list, struct process* proc)
+{
+	struct process* temp = list->head;
+	struct process* prev = NULL;
+	list->count -= 1;
+
+	if (list->head == proc)
+		list->head = NULL;
+	else {
+		while(temp != proc) {
+			prev = temp;
+			temp = temp->next;
+		}
+
+		prev->next = temp->next;
+		}
+	free(temp);
+}
 
 
 /* Print a list of all tasks currently being scheduled.  */
@@ -87,7 +186,14 @@ process_request(struct request_struct *rq)
 static void
 sigalrm_handler(int signum)
 {
-	assert(0 && "Please fill me!");
+	printf("ALARM!!\n");
+	if (signum != SIGALRM) {
+		fprintf(stderr, "Internal error: Called for signum %d, not SIGALRM\n",
+			signum);
+		exit(1);
+	}
+
+	kill(current->pPid, SIGSTOP);
 }
 
 /*
@@ -96,7 +202,71 @@ sigalrm_handler(int signum)
 static void
 sigchld_handler(int signum)
 {
-	assert(0 && "Please fill me!");
+	pid_t p;
+	int status;
+
+	if (signum != SIGCHLD) {
+		fprintf(stderr, "Internal error: Called for signum %d, not SIGCHLD\n",
+			signum);
+		exit(1);
+	}
+
+	/*
+	 * Something has happened to one of the children.
+	 * We use waitpid() with the WUNTRACED flag, instead of wait(), because
+	 * SIGCHLD may have been received for a stopped, not dead child.
+	 *
+	 * A single SIGCHLD may be received if many processes die at the same time.
+	 * We use waitpid() with the WNOHANG flag in a loop, to make sure all
+	 * children are taken care of before leaving the handler.
+	 */
+	for(;;) {
+		p = waitpid(-1, &status, WUNTRACED | WNOHANG);
+		if (p < 0) {
+			perror("waitpid");
+			exit(1);
+		}
+
+		if (p == 0)
+			break;
+
+		explain_wait_status(p, status);
+
+		if (WIFEXITED(status) || WIFSIGNALED(status)) {
+		/* A child has died */
+			struct process* temp = current;
+			current = current->next;
+			remove_proc_from_list(procList, temp);
+			if(procList->head == NULL) {
+				printf("All children finished. Exiting...\n");
+				exit(EXIT_SUCCESS);
+			}
+			if(current == NULL)
+					current = procList->head;
+
+			/* start alarm */
+			if (alarm(SCHED_TQ_SEC) < 0) {
+				perror("alarm");
+				exit(1);
+			}
+
+			kill(current->pPid, SIGCONT);
+		}
+
+		if (WIFSTOPPED(status)) {
+			/* A child has stopped due to SIGSTOP/SIGTSTP, etc... */
+			current = current->next;
+			if (current == NULL)
+				current = procList->head;
+
+			/* start alarm */
+			if (alarm(SCHED_TQ_SEC) < 0) {
+				perror("alarm");
+				exit(1);
+			}
+			kill(current->pPid, SIGCONT);
+		}
+	}
 }
 
 /* Disable delivery of SIGALRM and SIGCHLD. */
@@ -194,7 +364,7 @@ do_shell(char *executable, int wfd, int rfd)
  * two pipes are created for communication and passed
  * as command-line arguments to the executable.
  */
-static void
+static pid_t
 sched_create_shell(char *executable, int *request_fd, int *return_fd)
 {
 	pid_t p;
@@ -216,6 +386,8 @@ sched_create_shell(char *executable, int *request_fd, int *return_fd)
 		close(pfds_rq[0]);
 		close(pfds_ret[1]);
 		do_shell(executable, pfds_rq[1], pfds_ret[0]);
+
+		/* Unreachable */
 		assert(0);
 	}
 	/* Parent */
@@ -223,6 +395,8 @@ sched_create_shell(char *executable, int *request_fd, int *return_fd)
 	close(pfds_ret[0]);
 	*request_fd = pfds_rq[0];
 	*return_fd = pfds_ret[1];
+
+	return p;
 }
 
 static void
@@ -253,25 +427,69 @@ shell_request_loop(int request_fd, int return_fd)
 	}
 }
 
+static void
+child(void)
+{
+	char *newargv[] = { current->execName, NULL, NULL, NULL };
+	char *newenviron[] = { NULL };
+	execve(current->execName, newargv, newenviron);
+
+	/* Unreachable */
+	perror("execve:");
+	exit(1);
+}
+
+
 int main(int argc, char *argv[])
 {
 	int nproc;
+	pid_t p;
+	int i;
+
 	/* Two file descriptors for communication with the shell */
 	static int request_fd, return_fd;
 
+	/* Initialize the processes list */
+	procList = init_list();
+
+	/* Create a process for each of the given arguments */
+	for (i=1; i < argc; i++) {
+		current = init_process(i, argv[i]);
+		add_proc_to_list(procList, current);
+	}
+
 	/* Create the shell. */
-	sched_create_shell(SHELL_EXECUTABLE_NAME, &request_fd, &return_fd);
-	/* TODO: add the shell to the scheduler's tasks */
+	p = sched_create_shell(SHELL_EXECUTABLE_NAME, &request_fd, &return_fd);
 
-	/*
-	 * For each of argv[1] to argv[argc - 1],
-	 * create a new child process, add it to the process list.
-	 */
+	/* Add the shell to the scheduler's tasks */
+	current = init_process(i, SHELL_EXECUTABLE_NAME);
+	current->pPid = p;
+	add_proc_to_list(procList, current);
 
-	nproc = 0; /* number of proccesses goes here */
+
+	nproc = argc - 1; /* number of processes goes here */
+
+	current = procList->head;
+	for (i = 0; i < nproc; i++) {
+		p = fork();
+		if (p < 0) {
+			/* fork failed */
+			perror("fork");
+			exit(1);
+		}
+		else if (p == 0) {
+			raise(SIGSTOP);
+			child();
+		}
+		else  if (p > 0) {
+			current->pPid = p;
+			current = current->next;
+		}
+
+	}
 
 	/* Wait for all children to raise SIGSTOP before exec()ing. */
-	wait_for_ready_children(nproc);
+	wait_for_ready_children(nproc + 1);
 
 	/* Install SIGALRM and SIGCHLD handlers. */
 	install_signal_handlers();
@@ -280,6 +498,19 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Scheduler: No tasks. Exiting...\n");
 		exit(1);
 	}
+
+	current = procList->head;
+
+	/* start alarm */
+	if (alarm(SCHED_TQ_SEC) < 0) {
+		perror("alarm");
+		exit(1);
+	}
+
+	/* enable first process */
+	pid_t pid = current->pPid;
+	kill(pid, SIGCONT);
+
 
 	shell_request_loop(request_fd, return_fd);
 
